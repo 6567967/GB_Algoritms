0fa76b038dbf15fc21bd71d39dbb903a20fe30de
Ifl <6567967@gmail.com>
2019-01-02 12:55:15 +0300

    format

--- lesson2/task_5.py
+++ lesson2/task_5.py
@@ -3,7 +3,7 @@
 # Вывод выполнить в табличной форме: по десять пар "код-символ" в каждой строке.
 # https://drive.google.com/file/d/1dRO7fXCLV1LLGWruoj_rnoHU5a8pRIjH/view?usp=sharing
 
-for i in range(32,128):
+for i in range(32, 128):
     print(f"{i:3}-{chr(i)} ", end='')
     if (i-32+1) % 10 == 0:
         print("")
