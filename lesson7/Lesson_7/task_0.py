8e55b1092e98cef3e4e9c15f9cd54e16a14e6348
Ifl <6567967@gmail.com>
2019-02-09 13:15:47 +0300

    Revert "save"
    This reverts commit bc3603b4a76d993f00f12ee1fbc7f32f81c8b4ba.

--- lesson7/Lesson_7/task_0.py
+++ lesson7/Lesson_7/task_0.py
@@ -1,11 +1,6 @@
 import random
 
-for arr_len in range(11):
-    arr1 = [i for i in range(arr_len)]
-    arr2 = arr1.copy()
-    random.shuffle(arr2)
-    accum = 1
-    while  arr1 != arr2:
-        random.shuffle(arr2)
-        accum += 1
-    print(f"{accum} random.shufle  {arr_len}")
+array = [i for i in range(10)]
+print(array)
+random.shuffle(array)
+print(array)
